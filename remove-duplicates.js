f5d62382977f249e59c98b1f98fbeec9a0adc02f
indrasisdatta <indrasis.datta@yahoo.co.in>
2023-11-30 20:20:24 +0530

    Update remove-duplicates.js

--- remove-duplicates.js
+++ remove-duplicates.js
@@ -1,3 +1,24 @@
+/**
+ * Remove duplicates from array
+ * without using any in-built functions
+ */
+const removeDuplicatesArr = (arr) => {
+	let start = 0, end = start + 1, len = arr.length;
+  let result = [];
+ 	let checkMap = new Map();
+  for (let val of arr) {
+  	if (!checkMap.has(val)) {
+    	checkMap.set(val, true);
+      result.push(val)
+    }
+  }
+  return result;
+}
+
+const arr = [8, 2, 23, 12, 8, 1, 23, 42, 8];
+console.log('Result: ', removeDuplicatesArr(arr))
+
+
 const removeDuplicates = (arr) => {
   /* Store frequency of each no in this map */
   let elementMap = new Map();
