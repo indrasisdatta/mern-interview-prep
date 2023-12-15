3adc95caddad9ef247e7d27e84ee8149dedce62d
Indrasis Datta <indrasis.datta@yahoo.co.in>
2023-12-15 20:27:53 +0530

    Update generator-eg.js

--- generator-eg.js
+++ generator-eg.js
@@ -1,16 +1,18 @@
 function *generateId(len) {
 	let i = 1;
-  console.log('Gen before loop')
+  console.log('Gen before loop');
   while (i <= len) {
   	console.log('Gen before yield')
   	yield i;
     i++;
-    console.log('Gen after yield')
+    console.log('Gen after yield');
   }  
-  console.log('Gen after loop')
+  console.log('Gen after loop');
 }
 
-const id = generateId(3);
+const id = generateId(5);
+console.log(id.next())
+console.log(id.next())
 console.log(id.next())
 console.log(id.next())
 console.log(id.next())
