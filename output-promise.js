3d6e9f2faff0f04faae26261afa97ef9f8397fe4
Indrasis Datta <indrasis.datta@yahoo.co.in>
2023-12-21 20:04:16 +0530

    Promise output questions

---
+++ output-promise.js
@@ -0,0 +1,40 @@
+/* Eg. 1 */
+const customPromise = Promise.resolve('Promise data');
+
+const promiseCall1 = () => {
+	customPromise.then((res, rej) => {
+  	console.log('A', res);
+  })
+  console.log('A is called');
+}
+const promiseCall2 = async() => {
+	console.log('B', await customPromise);
+  console.log('B is called');
+}
+promiseCall1();
+promiseCall2();
+
+/* Eg. 2 */
+console.log(Promise.resolve(2)); // Returns Promise
+// To return value, either use .then or asnc/await
+(async() => {
+	console.log(await Promise.resolve(2))
+})()
+
+/* Eg. 3 */
+const promise1 = Promise.resolve('Promise 1');
+const promise2 = Promise.resolve('Promise 2');
+const promise3 = Promise.reject('Promise 3');
+const promise4 = Promise.resolve('Promise 4');
+
+const runPromise = async() => {
+  const res1 = await Promise.all([promise1, promise2]);
+   console.log('Res1', res1);
+  const res2 = await Promise.all([promise3, promise4]); 
+  console.log('Res2', res2)
+  return [res1, res2];
+}
+
+runPromise()
+	.then(res => console.log(res))
+	.catch(err => console.error(err))
