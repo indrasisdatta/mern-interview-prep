37b80c6cc8bae8b6afd2abec00fca6d097a679bb
Indrasis Datta <indrasis.datta@yahoo.co.in>
2024-03-12 19:42:35 +0530

    JS update billing end date function

--- js-code/billing-end-date.js
+++ js-code/billing-end-date.js
@@ -5,13 +5,30 @@
  * @param number durationMonths eg. 2
  *
  * Eg 1. Customer subscribed for 2 months, billed every 1 week
- *  billingFrequency = 1, durationMonths = 2.
+ *  billingFrequency = 1, durationMonths = 2. 
+ *  startDate = '2024-03-04'
  *  Expected output = 2024-04-29
  *
  * Eg 2. Customer subscribed for 6 months, billed every 2 weeks
- *  billingFrequency = 2, durationMonths = 6.
+ *  billingFrequency = 2, durationMonths = 6
+ *  startDate = '2024-03-04'
  *   Expected output = 2024-09-02
  */
- function getBillingEndDate(startDate, billingFrequency) {
- 
+ function getBillingEndDate(startDate, billingFrequency, durationMonths) {
+   let startTS = new Date(startDate).getTime();
+   const durationEndTS = addDays(new Date(startDate), durationMonths*30);
+   console.log(startTS, durationEndTS)
+   let endDate = null;
+   while (new Date(startDate).getTime() <= durationEndTS) {
+     endDate = new Date(startDate);
+       console.log('startDate', new Date(startDate))
+      startDate = addDays(new Date(startDate), billingFrequency*7);
+   }
+   console.log('End date', endDate)
  }
+ 
+ const addDays = (date, days) => {
+   return date.setDate(date.getDate() + days);
+ }
+ 
+ getBillingEndDate('2024-03-04', 1, 2); // 2024-04-29
