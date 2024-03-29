329e91fe1787872aefb520f17aa4073010d537ec
Indrasis Datta <indrasis.datta@yahoo.co.in>
2024-03-29 10:26:01 +0530

    Sum triplets hash map JS function

--- leetcode/sum-triplets.js
+++ leetcode/sum-triplets.js
@@ -41,3 +41,39 @@
   threeSum([-1,0,1,2,-1,-4]);
   threeSum([0,0,0]);
   threeSum([0,1,1]);
+
+/**
+ * Hash set logic
+ * (nums[i] + nums[j]) * (-1) -> check if already present
+ */
+ 
+ /**
+   * @param {number[]} nums
+   * @return {number[][]}
+   */
+  var threeSum = function(nums) {
+  	if (nums.length < 3) return [];
+    if (nums[0] === 0 && nums[1] === 0 && nums[2] === 0) return [ [0, 0, 0] ];
+    let len = nums.length, 
+    	  output = [], 
+        tempNos= [],
+        uniqueItems = new Map();
+    for (let i = 0; i < len - 1; i++) {
+      for (let j = i; j < len - 2; j++) {
+      	let item = (nums[i] + nums[j]) * (-1);
+      	if (uniqueItems.has(item) && !tempNos.includes(item)) {
+        	tempNos.push(item);
+        	output.push([nums[i], nums[j], item]);
+        } else {
+        	uniqueItems.set(nums[i]);
+          uniqueItems.set(nums[j]);
+        }
+      }
+    }
+    console.log('Output: ', output);
+    return output;
+  };
+    
+  threeSum([-1,0,1,2,-1,-4]);
+  threeSum([0,0,0]);
+  threeSum([0,1,1]);
