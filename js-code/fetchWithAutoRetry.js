8fa184588346a351597349cae231f0a9cfa8055a
Indrasis Datta <indrasis.datta@yahoo.co.in>
2024-05-27 08:47:34 +0530

    JS function fetchWithAutoRetry solution

--- js-code/fetchWithAutoRetry.js
+++ js-code/fetchWithAutoRetry.js
@@ -3,6 +3,33 @@
  * which automatically fetch again when error happens, 
  * until the maximum count is met.
  */
-function fetchWithAutoRetry(fetcher, maximumRetryCount) {
-    // code goes here
+async function fetchWithAutoRetry(fetcher, maximumRetryCount) {
+     console.log(`fetchWithAutoRetry ${maximumRetryCount}`, fetcher);
+    try {
+        console.log(`Try block attempt ${maximumRetryCount}`);
+    	const tempFetcher = await fetcher(maximumRetryCount);
+    	return tempFetcher;
+    } catch (e) {
+    	console.error(`Catch block Retry #{maximumRetryCount} - Error caught: `, e);
+        return fetchWithAutoRetry(fetcher, maximumRetryCount-1);
+    }
 }
+
+const fetcher = (retryCount) => {
+  return new Promise((resolve, reject) => {
+      if (retryCount === 2) resolve('Resolved value 2')
+      reject('Rejected value')
+  })
+}
+
+(async() => {
+    try {
+         const result = await fetchWithAutoRetry(fetcher, 5);
+         console.log('Final result: ', result)
+    } catch (e) {
+        console.error('Main async error: ', e)
+    }
+})();
+
+
+
