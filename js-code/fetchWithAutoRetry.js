670866fa2a873a55d615722d5c3183e475106754
Indrasis Datta <indrasis.datta@yahoo.co.in>
2024-05-27 08:15:27 +0530

    JS function fetchAutoRetry

---
+++ js-code/fetchWithAutoRetry.js
@@ -0,0 +1,8 @@
+/** 
+ * Create a fetchWithAutoRetry(fetcher, count)
+ * which automatically fetch again when error happens, 
+ * until the maximum count is met.
+ */
+function fetchWithAutoRetry(fetcher, maximumRetryCount) {
+    // code goes here
+}
