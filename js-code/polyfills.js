eec698851ec5e9b5dd22740923782d1b5ffacb0a
Indrasis Datta <indrasis.datta@yahoo.co.in>
2025-10-14 09:43:16 +0530

    Js polyfills - call, apply, bind

--- js-code/polyfills.js
+++ js-code/polyfills.js
@@ -4,7 +4,8 @@
  * Eg. map, reduce were introduced in ES2015, so browsers using older verions of JS won't support these functions. 
  * We can write polyfills so that these functions are supported in old browsers.
  * 
- * map, filter, reduce, call, apply, bind, 
+ * map, filter, reduce, 
+ * call, apply, bind, 
  * promise, promise.all(), 
  * debounce, throttle, once, memoize, 
  */
@@ -56,3 +57,66 @@ Array.prototype.myReduce = function(callbackFn, initialVal) {
 }
 
 console.log(arr.myReduce((acc, cur, i, arr) => acc + cur, 0))
+
+/**
+ * 4. call polyfill
+ * calculateInterest.call(savingsBank, 'John', '$')
+ */
+Function.prototype.myCall = function(context = {}, ...args) {
+    if (typeof this !== "function") {
+        throw new Error("Not callable function");
+    }
+    context.fn = this;
+    context.fn(...args);
+}
+
+const savingsBank = {
+  principal: 10000,
+  rate: 0.05,
+  time: 2
+};
+function calculateInterest(customerName, currency) {
+  const interest = this.principal * this.rate * this.time / 100;
+  console.log(`${customerName} received interest of ${currency}${interest}`)
+}
+
+// calculateInterest.call(savingsBank, 'John', '$')
+calculateInterest.myCall(savingsBank, 'John', '$')
+
+/**
+ * 5. apply Polyfill
+ * calculateInterest.apply(savingsBank, ['John', '$'])
+ */
+Function.prototype.myApply = function(context = {}, args=[]) {
+    if (typeof this !== "function") {
+        throw new Error("Not callable function");
+    }
+    if (!Array.isArray(args)) {
+      throw new Error("Expected args array");
+    }
+    context.fn = this;
+    context.fn(...args);
+}
+calculateInterest.myApply(savingsBank, ['John', '$'])
+
+/**
+ * 6. bind Polyfill
+ * const showInterest = calculateInterest.bind(savingsBank, 'John', '$');
+    showInterest();
+    OR
+    const showInterest2 = calculateInterest.bind(savingsBank);
+    showInterest2('Jane', '$');
+    const showInterest2 = calculateInterest.bind(savingsBank, 'Will');
+    showInterest2('$');
+ */
+Function.prototype.myBind = function(context = {}, ...args) {
+    if (typeof this !== "function") {
+        throw new Error("myBind is not callable")
+    }
+    context.fn = this;    
+    return function(...newArgs) {
+        context.fn(...args, ...newArgs);
+    }
+}
+const showInterest = calculateInterest.myBind(savingsBank, 'Will');
+showInterest('$');
